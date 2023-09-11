36eeb44b06aea773946b84c7d28a911a08eb6c28
Frank Carey <fcarey@gmail.com>
2023-09-11 12:40:02 -0400

    SIM-2496: Setup server based on aiohttp socketio example. Works with Unity.

---
+++ fable_agents/server.py
@@ -0,0 +1,29 @@
+from aiohttp import web
+import socketio
+
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
+
+async def index(request):
+    """Serve the client-side application."""
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
+
+@sio.event
+def connect(sid, environ):
+    print("connect ", sid)
+
+@sio.event
+async def chat_message(sid, data):
+    print("message ", data)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+app.router.add_static('/static', 'static')
+app.router.add_get('/', index)
+
+if __name__ == '__main__':
+    web.run_app(app)
